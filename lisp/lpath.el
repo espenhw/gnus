c622638ad21643441b846f6466e3bc0bdc63c286
ShengHuo ZHU <zsh@cs.rochester.edu>
2000-04-28 05:03:23 +0000

    Fbound put-image create-image display-graphic-p.

--- lisp/lpath.el
+++ lisp/lpath.el
@@ -43,7 +43,7 @@
 		     temp-directory babel-fetch babel-wash
 		     find-coding-systems-for-charsets sc-cite-regexp
 		     vcard-pretty-print image-type-available-p
-		     put-image 
+		     put-image create-image  display-graphic-p
 		     make-overlay overlay-put))
       (maybe-bind '(global-face-data
 		    mark-active transient-mark-mode mouse-selection-click-count
@@ -96,6 +96,7 @@
 		 rmail-summary-exists rmail-select-summary rmail-update-summary
 		 url-generic-parse-url valid-image-instantiator-format-p
 		 babel-fetch babel-wash babel-as-string sc-cite-regexp
+		 put-image create-image display-graphic-p
 		 vcard-pretty-print image-type-available-p)))
 
 (setq load-path (cons "." load-path))
