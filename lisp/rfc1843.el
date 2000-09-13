7462983373b6282d2f2e4c87d7716197c648a4da
Dave Love <fx@gnu.org>
2000-09-13 16:16:30 +0000

    Update header keywords.

--- lisp/rfc1843.el
+++ lisp/rfc1843.el
@@ -2,7 +2,7 @@
 ;; Copyright (c) 1998, 1999, 2000 Free Software Foundation, Inc.
 
 ;; Author: Shenghuo Zhu <zsh@cs.rochester.edu>
-;; Keywords: news HZ HZ+
+;; Keywords: news HZ HZ+ mail i18n
 
 ;; This file is a part of GNU Emacs.
 
