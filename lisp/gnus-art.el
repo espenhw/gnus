cadffc73323ee757f790606a5654a7d5cf10d510
ShengHuo ZHU <zsh@cs.rochester.edu>
2001-01-12 19:58:45 +0000

    Thirteenth.

--- lisp/gnus-art.el
+++ lisp/gnus-art.el
@@ -2259,7 +2259,7 @@ should replace the \"Date:\" one, or should be added below it."
 	 (number-to-string (nth 3 dtime))
 	 (let ((digit (% (nth 3 dtime) 10)))
 	   (cond
-	    ((memq (nth 3 dtime) '(11 12)) "th")
+	    ((memq (nth 3 dtime) '(11 12 13)) "th")
 	    ((= digit 1) "st")
 	    ((= digit 2) "nd")
 	    ((= digit 3) "rd")
