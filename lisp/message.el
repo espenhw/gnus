3f5dc7fa357d8405688974c3cffaa65294c01d95
Simon Josefsson <jas@extundo.com>
2001-01-19 18:39:45 +0000

    2001-01-19  Simon Josefsson  <sj@extundo.com>
    	* message.el (message-ignored-news-headers): Only search beginning
    	of line.

--- lisp/message.el
+++ lisp/message.el
@@ -202,7 +202,7 @@ included.  Organization, Lines and User-Agent are optional."
   :type 'sexp)
 
 (defcustom message-ignored-news-headers
-  "^NNTP-Posting-Host:\\|^Xref:\\|^[BGF]cc:\\|^Resent-Fcc:\\|X-Draft-From:"
+  "^NNTP-Posting-Host:\\|^Xref:\\|^[BGF]cc:\\|^Resent-Fcc:\\|^X-Draft-From:"
   "*Regexp of headers to be removed unconditionally before posting."
   :group 'message-news
   :group 'message-headers
