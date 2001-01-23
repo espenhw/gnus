62b5df34967cc6e21bafb46e4239f83fd225e423
ShengHuo ZHU <zsh@cs.rochester.edu>
2001-01-23 16:12:28 +0000

    2001-01-23 11:00:00  ShengHuo ZHU  <zsh@cs.rochester.edu>
    	* gnus-sum.el (gnus-summary-print-article): Remove process mark.

--- lisp/gnus-sum.el
+++ lisp/gnus-sum.el
@@ -1,5 +1,5 @@
 ;;; gnus-sum.el --- summary mode commands for Gnus
-;; Copyright (C) 1996, 1997, 1998, 1999, 2000
+;; Copyright (C) 1996, 1997, 1998, 1999, 2000, 2001
 ;;        Free Software Foundation, Inc.
 
 ;; Author: Lars Magne Ingebrigtsen <larsi@gnus.org>
@@ -7429,7 +7429,8 @@ to save in."
 		(gnus-run-hooks 'gnus-ps-print-hook)
 		(save-excursion
 		  (ps-spool-buffer-with-faces))))
-	  (kill-buffer buffer)))))
+	  (kill-buffer buffer))))
+    (gnus-summary-remove-process-mark article))
   (ps-despool filename))
 
 (defun gnus-summary-show-article (&optional arg)
