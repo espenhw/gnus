906b4006b4f9a816a87dea4704dd921b2466d986
Simon Josefsson <jas@extundo.com>
2001-07-01 15:58:36 +0000

    2001-07-01  Simon Josefsson  <jas@extundo.com>
    	* nnimap.el (nnimap-date-days-ago): Defeat locale.

--- lisp/nnimap.el
+++ lisp/nnimap.el
@@ -1103,10 +1103,12 @@ function is generally only called when Gnus is shutting down."
 
 (defun nnimap-date-days-ago (daysago)
   "Return date, in format \"3-Aug-1998\", for DAYSAGO days ago."
-  (let ((date (format-time-string "%d-%b-%Y"
-				  (nnimap-time-substract
-				   (current-time)
-				   (days-to-time daysago)))))
+  (let* ((time (nnimap-time-substract (current-time) (days-to-time daysago)))
+	 (date (format-time-string
+		(format "%%d-%s-%%Y"
+			(capitalize (car (rassoc (nth 4 (decode-time time))
+						 parse-time-months))))
+		time)))
     (if (eq ?0 (string-to-char date))
 	(substring date 1)
       date)))
