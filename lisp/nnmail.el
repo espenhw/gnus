d934eb659d2484a9c4917aacc0afa72afce7ac8c
Kai Grossjohann <kgrossjo@eu.uu.net>
2001-08-04 14:43:48 +0000

    (nnmail-split-fancy-with-parent): Correct `mapconcat' syntax.  Protect string-match against nil string and regexp.

--- lisp/nnmail.el
+++ lisp/nnmail.el
@@ -1496,7 +1496,7 @@ See the Info node `(gnus)Fancy Mail Splitting' for more details."
       (mapcar (lambda (x)
 		(setq res (or (nnmail-cache-fetch-group x) res))
 		(when (or (string= "drafts" res)
-			  (and regexp (string-match regexp res)))
+			  (and regexp res (string-match regexp res)))
 		  (setq res nil)))
 	      references)
       res)))
