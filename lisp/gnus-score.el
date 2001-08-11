567ac901c1b411449d29448bac8fdd859a1b3473
Kai Grossjohann <kgrossjo@eu.uu.net>
2001-08-11 16:12:21 +0000

    Fix from Karl Kleinpaste.

--- lisp/gnus-score.el
+++ lisp/gnus-score.el
@@ -1952,7 +1952,7 @@ score in GNUS-NEWSGROUP-SCORED by SCORE."
 	  ;; Evil hackery to make match usable in non-standard headers.
 	  (when extra
 	    (setq match (concat "[ (](" extra " \\. \"[^)]*"
-				match "[^(]*\")[ )]")
+				match "[^\"]*\")[ )]")
 		  search-func 're-search-forward)) ; XXX danger?!?
 
 	  (cond
