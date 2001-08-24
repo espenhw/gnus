82e97ac7c718afdbf2630a8ae999ed9749ecd5a7
ShengHuo ZHU <zsh@cs.rochester.edu>
2001-08-24 04:43:11 +0000

    2001-08-23 21:00:00  ShengHuo ZHU  <zsh@cs.rochester.edu>
    	* gnus-util.el (gnus-create-info-command): Return an interactive
    	function.

--- lisp/gnus-util.el
+++ lisp/gnus-util.el
@@ -1064,6 +1064,7 @@ sure of changing the value of `foo'."
 (defun gnus-create-info-command (node)
   "Create a command that will go to info NODE."
   `(lambda ()
+     (interactive)
      ,(concat "Enter the info system at node " node)
      (Info-goto-node ,node)
      (setq gnus-info-buffer (current-buffer))
