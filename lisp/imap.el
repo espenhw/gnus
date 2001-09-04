208752ff0259c0c62d6c17237e13a03b9961d877
Simon Josefsson <jas@extundo.com>
2001-09-04 17:29:43 +0000

    Sigh, fix another typo in last commit:
    2001-09-04  Simon Josefsson  <jas@extundo.com>
    
    	* imap.el (imap-search): Don't error if server is broken.

--- lisp/imap.el
+++ lisp/imap.el
@@ -1422,7 +1422,9 @@ is non-nil return theese properties."
     (imap-mailbox-put 'search 'dummy)
     (when (imap-ok-p (imap-send-command-wait (concat "UID SEARCH " predicate)))
       (if (eq (imap-mailbox-get-1 'search imap-current-mailbox) 'dummy)
-	  (message "Missing SEARCH response to a SEARCH command (server not RFC compliant)...")
+	  (progn
+	    (message "Missing SEARCH response to a SEARCH command (server not RFC compliant)...")
+	    nil)
 	(imap-mailbox-get-1 'search imap-current-mailbox)))))
 
 (defun imap-message-flag-permanent-p (flag &optional mailbox buffer)
