0429ff01ab304bb434566f899bee509118f1fe87
Kai Grossjohann <kgrossjo@eu.uu.net>
2001-10-19 17:58:23 +0000

    Use correct directory structure for XEmacs on Windows.

--- make-x.bat
+++ make-x.bat
@@ -1,5 +1,6 @@
 @echo off
 
+rem Modified once more by Frank Schmitt (ich@Frank-Schmitt.net)
 rem Modified by ShengHuo Zhu (zsh@cs.rochester.edu)
 rem Originally from make.bat by David Charlap (shamino@writeme.com)
 rem
@@ -9,43 +10,41 @@ set GNUS_PWD_SAVE=%PWD%
 set PWD=
 
 if "%1" == "" goto usage
-if "%2" == "" goto usage
 
 set emacs=xemacs.exe
 
 cd lisp
-call %1\%2\%emacs% -batch -q -no-site-file -l ./dgnushack.el -f dgnushack-compile
-if not "%3" == "copy" goto info
-attrib -r %1\lisp\gnus\*
-copy *.el* %1\lisp\gnus
+call %1\%emacs% -batch -nw -q -no-site-file -l ./dgnushack.el -f dgnushack-compile
+if not "%2" == "copy" goto info
+attrib -r %1\..\..\xemacs-packages\lisp\gnus\*.*
+copy *.el* %1\..\..\xemacs-packages\lisp\gnus
 
 :info
-set EMACSINFO=call %1\%2\%emacs% -no-site-file -no-init-file -batch -q -l infohack.el -f batch-makeinfo
+set EMACSINFO=call %1\%emacs% -no-site-file -no-init-file -batch -nw -q -l infohack.el -f batch-makeinfo
 cd ..\texi
 %EMACSINFO% message.texi
 %EMACSINFO% emacs-mime.texi
 %EMACSINFO% gnus.texi
-if not "%3" == "copy" goto done
-copy gnus %1\info
-copy gnus-?? %1\info
-copy message %1\info
-copy emacs-mime %1\info
+if not "%2" == "copy" goto done
+copy gnus %1\..\..\xemacs-packages\info
+copy gnus-?? %1\..\..\xemacs-packages\info
+copy message %1\..\..\xemacs-packages\info
+copy emacs-mime %1\..\..\xemacs-packages\info
 
 :etc
 cd ..\etc
-copy gnus-tut.txt %1\etc
+copy gnus-tut.txt %1\..\..\xemacs-packages\etc
 
 :done
 cd ..
 goto end
 
 :usage
-echo Usage: make :xemacs-dir: :xemacs-arch: [copy]
+echo Usage: make :xemacs-dir: [copy]
 echo.
-echo where: :xemacs-dir: is the directory you installed xemacs in
-echo                    eg. C:\Progra~1\XEmacs\XEmacs-21.4.3
-echo        :xemacs-arch: is the xemacs architecture you installed
-echo                    eg. i586-pc-win32
+echo where: :xemacs-dir: is the directory you installed xemacs in 
+echo                     (the directory where xemacs.exe is situated)
+echo                    eg. C:\Programme\XEmacs\XEmacs-21.4.3\i586-pc-win32
 echo        copy indicates that the compiled files should be copied to your
 echo             emacs lisp, info, and etc directories
 echo.
