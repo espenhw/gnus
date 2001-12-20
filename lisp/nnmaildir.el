e6c35a20a80021260e2ba160c594b6ce35953238
ShengHuo ZHU <zsh@cs.rochester.edu>
2001-12-20 20:40:32 +0000

    2001-12-20 15:00:00  ShengHuo ZHU  <zsh@cs.rochester.edu>
    	* nnmaildir.el: Copyright changes. Require cl only at compile time.

--- lisp/nnmaildir.el
+++ lisp/nnmaildir.el
@@ -1,12 +1,33 @@
-;;; nnmaildir.el: maildir backend for Gnus                   -*- emacs-lisp -*-
+;;; nnmaildir.el --- maildir backend for Gnus
+;; Copyright (c) 2001 Free Software Foundation, Inc.
+;; Copyright (c) 2000, 2001 Paul Jarc <prj@po.cwru.edu>
+
+;; Author: Paul Jarc <prj@po.cwru.edu>
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
 ;;; <URL:http://multivac.cwru.edu./nnmaildir/>
-;;; This software is copyright by Paul Jarc <prj@po.cwru.edu>.  It is
-;;; entirely devoid of warranty.  Permission is granted to everyone to
-;;; use, modify, copy, and redistribute it as specified by the GNU
-;;; General Public License, version 2 or (if desired) any later version.
+
+;;; Code:
 
 (eval-and-compile
-  (require 'cl)
   (require 'nnheader)
   (require 'gnus)
   (require 'gnus-util)
@@ -15,6 +36,7 @@
   (require 'gnus-int)
   (require 'message))
 (eval-when-compile
+  (require 'cl)
   (require 'nnmail))
 
 (gnus-declare-backend "nnmaildir" 'mail 'respool 'address)
@@ -1638,3 +1660,5 @@ by nnmaildir-request-article.")
   t)
 
 (provide 'nnmaildir)
+
+;;; nnmaildir.el ends here
