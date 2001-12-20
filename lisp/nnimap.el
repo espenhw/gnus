d49066cc0edab5eb9765333159e548c76fd67ced
Simon Josefsson <jas@extundo.com>
2001-12-20 17:53:28 +0000

    2001-12-20  Simon Josefsson  <jas@extundo.com>
    	* nnimap.el (top-level): Don't require cl.  Suggested by ShengHuo
    	ZHU <zsh@cs.rochester.edu>.
    	(nnimap-close-group): Don't quote KEYLIST items.  Suggested by
    	Brian P Templeton <bpt@tunes.org>.

--- lisp/nnimap.el
+++ lisp/nnimap.el
@@ -58,10 +58,7 @@
 
 ;;; Code:
 
-(eval-and-compile
-  (require 'cl)
-  (require 'imap))
-
+(require 'imap)
 (require 'nnoo)
 (require 'nnmail)
 (require 'nnheader)
@@ -846,9 +843,9 @@ function is generally only called when Gnus is shutting down."
     (when (and (imap-opened)
 	       (nnimap-possibly-change-group group server))
       (case nnimap-expunge-on-close
-	('always (imap-mailbox-expunge nnimap-close-asynchronous)
+	(always (imap-mailbox-expunge nnimap-close-asynchronous)
 		 (imap-mailbox-close nnimap-close-asynchronous))
-	('ask (if (and (imap-search "DELETED")
+	(ask (if (and (imap-search "DELETED")
 		       (gnus-y-or-n-p (format
 				       "Expunge articles in group `%s'? "
 				       imap-current-mailbox)))
