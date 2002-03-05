3bfea13e662f8293f6738c73be6218138196486d
Paul Jarc <prj@po.cwru.edu>
2002-03-05 23:09:34 +0000

    * nnnil.el: New file. * gnus.el (gnus-valid-select-methods): Include nnnil.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -1438,7 +1438,8 @@ slower."
     ("nnlistserv" none)
     ("nnagent" post-mail)
     ("nnimap" post-mail address prompt-address physical-address)
-    ("nnmaildir" mail respool address))
+    ("nnmaildir" mail respool address)
+    ("nnnil" none))
   "*An alist of valid select methods.
 The first element of each list lists should be a string with the name
 of the select method.  The other elements may be the category of
