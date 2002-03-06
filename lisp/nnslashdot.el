7eb6e38a56fac169b813b32eaa3b627a1bba24d9
ShengHuo ZHU <zsh@cs.rochester.edu>
2002-03-06 21:17:41 +0000

    * nnslashdot.el (nnslashdot-request-article): Use "<!-- no ad 6 	-->" as the end of the first article.

--- lisp/nnslashdot.el
+++ lisp/nnslashdot.el
@@ -252,7 +252,7 @@
 			   (point)
 			   (progn
 			     (re-search-forward
-			      "&lt;&nbsp;[ \t\r\n]*<A HREF=\"\\(\\(http:\\)?//slashdot\\.org\\)?/article")
+			      "<!-- no ad 6 -->\\|&lt;&nbsp;[ \t\r\n]*<A HREF=\"\\(\\(http:\\)?//slashdot\\.org\\)?/article")
 			     (match-beginning 0)))))
 		(setq cid (cdr (assq article
 				     (nth 4 (assoc group nnslashdot-groups)))))
