d17c46739f9150318183dc04cc5865c119ec596a
Kai Grossjohann <kgrossjo@eu.uu.net>
2002-06-05 10:02:36 +0000

    Whitespace changes.

--- lisp/message.el
+++ lisp/message.el
@@ -2994,29 +2994,29 @@ If you always want Gnus to send messages in one piece, set
     (let ((default-directory "/")
 	  (coding-system-for-write message-send-coding-system))
       (let ((cpr
-      (apply 'call-process-region
-	     (append (list (point-min) (point-max)
-			   (if (boundp 'sendmail-program)
-			       sendmail-program
-			     "/usr/lib/sendmail")
-			   nil errbuf nil "-oi")
-		     ;; Always specify who from,
-		     ;; since some systems have broken sendmails.
-		     ;; But some systems are more broken with -f, so
-		     ;; we'll let users override this.
-		     (if (null message-sendmail-f-is-evil)
-			 (list "-f" (message-make-address)))
-		     ;; These mean "report errors by mail"
-		     ;; and "deliver in background".
-		     (if (null message-interactive) '("-oem" "-odb"))
-		     ;; Get the addresses from the message
-		     ;; unless this is a resend.
-		     ;; We must not do that for a resend
-		     ;; because we would find the original addresses.
-		     ;; For a resend, include the specific addresses.
-		     (if resend-to-addresses
-			 (list resend-to-addresses)
-		       '("-t"))))))
+	     (apply 'call-process-region
+		    (append (list (point-min) (point-max)
+				  (if (boundp 'sendmail-program)
+				      sendmail-program
+				    "/usr/lib/sendmail")
+				  nil errbuf nil "-oi")
+			    ;; Always specify who from,
+			    ;; since some systems have broken sendmails.
+			    ;; But some systems are more broken with -f, so
+			    ;; we'll let users override this.
+			    (if (null message-sendmail-f-is-evil)
+				(list "-f" (message-make-address)))
+			    ;; These mean "report errors by mail"
+			    ;; and "deliver in background".
+			    (if (null message-interactive) '("-oem" "-odb"))
+			    ;; Get the addresses from the message
+			    ;; unless this is a resend.
+			    ;; We must not do that for a resend
+			    ;; because we would find the original addresses.
+			    ;; For a resend, include the specific addresses.
+			    (if resend-to-addresses
+				(list resend-to-addresses)
+			      '("-t"))))))
 	(unless (zerop cpr)
 	  (error "Sending...failed: %s" cpr))))
     (when message-interactive
