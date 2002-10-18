4f0276fe7fef04062a3349024aaff38e1ac4a252
Katsumi Yamaoka <yamaoka@jpl.org>
2002-10-18 13:37:42 +0000

    (message-send-mail): Make it possible to perform edebug-defun.

--- lisp/message.el
+++ lisp/message.el
@@ -3369,13 +3369,14 @@ It should typically alter the sending method in some way or other."
 			"The message size is too large, split? "
 			t
 			"\
-The message size, " (/ (point-max) 1000) "KB, is too large.
+The message size, "
+			(/ (point-max) 1000) "KB, is too large.
 
 Some mail gateways (MTA's) bounce large messages.  To avoid the
 problem, answer `y', and the message will be split into several
 smaller pieces, the size of each is about "
-(/ message-send-mail-partially-limit 1000)
-"KB except the last
+			(/ message-send-mail-partially-limit 1000)
+			"KB except the last
 one.
 
 However, some mail readers (MUA's) can't read split messages, i.e.,
