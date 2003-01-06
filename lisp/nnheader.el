0b8065c654ce24731b8b64116e5c755fe560d99c
Kevin Greiner <kevin.greiner@compsol.cc>
2003-01-06 07:13:56 +0000

    * nnheader.el (nnheader-insert-nov-file): Do not try to insert an empty file as the parser assumes that the file isn't empty.

--- lisp/nnheader.el
+++ lisp/nnheader.el
@@ -909,21 +909,22 @@ find-file-hooks, etc.
 (defun nnheader-insert-nov-file (file first)
   (let ((size (nth 7 (file-attributes file)))
 	(cutoff (* 32 1024)))
-    (if (< size cutoff)
-	;; If the file is small, we just load it.
-	(nnheader-insert-file-contents file)
-      ;; We start on the assumption that FIRST is pretty recent.  If
-      ;; not, we just insert the rest of the file as well.
-      (let (current)
-	(nnheader-insert-file-contents file nil (- size cutoff) size)
-	(goto-char (point-min))
-	(delete-region (point) (or (search-forward "\n" nil 'move) (point)))
-	(setq current (ignore-errors (read (current-buffer))))
-	(if (and (numberp current)
-		 (< current first))
-	    t
-	  (delete-region (point-min) (point-max))
-	  (nnheader-insert-file-contents file))))))
+    (when size
+      (if (< size cutoff)
+          ;; If the file is small, we just load it.
+          (nnheader-insert-file-contents file)
+        ;; We start on the assumption that FIRST is pretty recent.  If
+        ;; not, we just insert the rest of the file as well.
+        (let (current)
+          (nnheader-insert-file-contents file nil (- size cutoff) size)
+          (goto-char (point-min))
+          (delete-region (point) (or (search-forward "\n" nil 'move) (point)))
+          (setq current (ignore-errors (read (current-buffer))))
+          (if (and (numberp current)
+                   (< current first))
+              t
+            (delete-region (point-min) (point-max))
+            (nnheader-insert-file-contents file)))))))
 
 (defun nnheader-find-file-noselect (&rest args)
   (let ((format-alist nil)
