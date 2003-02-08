5bb611b7b55b59ea91e9c627734cec6ed1452f09
Lars Magne Ingebrigtsen <larsi@gnus.org>
2003-02-08 22:33:48 +0000

    (gnus-version-number): Bumped.

--- lisp/gnus.el
+++ lisp/gnus.el
@@ -286,7 +286,7 @@ is restarted, and sometimes reloaded."
   :link '(custom-manual "(gnus)Exiting Gnus")
   :group 'gnus)
 
-(defconst gnus-version-number "0.15"
+(defconst gnus-version-number "0.16"
   "Version number for this version of Gnus.")
 
 (defconst gnus-version (format "Oort Gnus v%s" gnus-version-number)
