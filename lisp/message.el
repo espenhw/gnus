772cc5cd73984c68c046f8d07bdeda7a70e8a32c
Kai Grossjohann <kgrossjo@eu.uu.net>
2003-02-23 10:54:08 +0000

    (message-mode): \\(...\\) around additional paragraph-separate alternative.

--- lisp/message.el
+++ lisp/message.el
@@ -2270,9 +2270,11 @@ M-RET    `message-newline-and-reformat' (break the line and reformat)."
   (set (make-local-variable 'message-checksum) nil)
   (set (make-local-variable 'message-mime-part) 0)
   (message-setup-fill-variables)
-  (set (make-local-variable 'paragraph-separate)
-       (concat paragraph-separate
-	       "\\|<#!*/?\\(multipart\\|part\\|external\\|mml\\|secure\\)"))
+  (set
+   (make-local-variable 'paragraph-separate)
+   (format "\\(%s\\)\\|\\(%s\\)"
+	   paragraph-separate
+	   "<#!*/?\\(multipart\\|part\\|external\\|mml\\|secure\\)"))
   ;; Allow using comment commands to add/remove quoting.
   (set (make-local-variable 'comment-start) message-yank-prefix)
   (if (featurep 'xemacs)
