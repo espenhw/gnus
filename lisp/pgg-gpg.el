d2725612c428d3469da5d8933ecd79cb258a589f
Jesper Harder <harder@ifa.au.dk>
2003-04-03 02:39:52 +0000

    (pgg-gpg-process-region): do.

--- lisp/pgg-gpg.el
+++ lisp/pgg-gpg.el
@@ -47,9 +47,7 @@
   "GnuPG ID of your default identity.")
 
 (defun pgg-gpg-process-region (start end passphrase program args)
-  (let* ((output-file-name
-	  (expand-file-name (make-temp-name "pgg-output") 
-			    pgg-temporary-file-directory))
+  (let* ((output-file-name (pgg-make-temp-file "pgg-output"))
 	 (args
 	  `("--status-fd" "2"
 	    ,@(if passphrase '("--passphrase-fd" "0"))
