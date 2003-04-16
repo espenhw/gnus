c7abc0b44a551f33b08b492db17d2d7cf6925899
Dave Love <fx@gnu.org>
2003-04-16 14:23:54 +0000

    (gnus-summary-prepare-threads): Don't use copy-list. (gnus-summary-insert-articles): Use gnus-merge.

--- lisp/gnus-sum.el
+++ lisp/gnus-sum.el
@@ -2134,9 +2134,9 @@ increase the score of each group you read."
 	       ,@(gnus-summary-menu-split
 		  (mapcar
 		   (lambda (cs)
-		     ;; Since easymenu under FSF Emacs doesn't allow lambda
-		     ;; forms for menu commands, we should provide intern'ed
-		     ;; function symbols.
+		     ;; Since easymenu under Emacs doesn't allow
+		     ;; lambda forms for menu commands, we should
+		     ;; provide intern'ed function symbols.
 		     (let ((command (intern (format "\
 gnus-summary-show-article-from-menu-as-charset-%s" cs))))
 		       (fset command
@@ -4870,7 +4870,7 @@ or a straight list of headers."
 
 	(when (nth 1 thread)
 	  (push (list (max 0 gnus-tmp-level)
-		      (copy-list tree-stack)
+		      (copy-sequence tree-stack)
 		      (nthcdr 1 thread))
 		stack))
 	(push (if (nth 1 thread) 1 0) tree-stack)
@@ -11431,10 +11431,10 @@ returned."
 						(mail-header-number h))
 					      gnus-newsgroup-headers)))
     (setq gnus-newsgroup-headers
-	  (merge 'list
-		 gnus-newsgroup-headers
-		 (gnus-fetch-headers articles)
-		 'gnus-article-sort-by-number))
+	  (gnus-merge 'list
+		      gnus-newsgroup-headers
+		      (gnus-fetch-headers articles)
+		      'gnus-article-sort-by-number))
     ;; Suppress duplicates?
     (when gnus-suppress-duplicates
       (gnus-dup-suppress-articles))
