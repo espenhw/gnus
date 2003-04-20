b164ff305a0c1011e23c55614de419f0c6fac860
Simon Josefsson <jas@extundo.com>
2003-04-20 11:57:11 +0000

    * mml-sec.el: Fix license template.
    * gnus-sieve.el, sieve.el, sieve-manage.el, sieve-mode.el: Fix
    license template.

--- lisp/mml-sec.el
+++ lisp/mml-sec.el
@@ -2,7 +2,8 @@
 ;; Copyright (C) 2000, 2001, 2002, 2003 Free Software Foundation, Inc.
 
 ;; Author: Simon Josefsson <simon@josefsson.org>
-;; This file is not part of GNU Emacs, but the same permissions apply.
+
+;; This file is part of GNU Emacs.
 
 ;; GNU Emacs is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
