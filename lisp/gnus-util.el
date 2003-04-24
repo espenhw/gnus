38387fb9a6a0aeae1ff60c0658a876c7c94faab7
Dave Love <fx@gnu.org>
2003-04-24 18:26:55 +0000

    (gnus-use-byte-compile, gnus-url-unhex-string): Doc fix. (gnus-merge): Revert last change (caller fixed).

--- lisp/gnus-util.el
+++ lisp/gnus-util.el
@@ -104,11 +104,14 @@
 ;; Added by Geoffrey T. Dairiki <dairiki@u.washington.edu>.  A safe way
 ;; to limit the length of a string.  This function is necessary since
 ;; `(substr "abc" 0 30)' pukes with "Args out of range".
+;; Fixme: Why not `truncate-string-to-width'?
 (defsubst gnus-limit-string (str width)
   (if (> (length str) width)
       (substring str 0 width)
     str))
 
+;; Fixme: This is incomplete, but why not just use functionp?  In
+;; fact, that is used in various places.
 (defsubst gnus-functionp (form)
   "Return non-nil if FORM is funcallable."
   (or (and (symbolp form) (fboundp form))
@@ -1026,6 +1029,7 @@ Return the modified alist."
       (while (search-backward "\\." nil t)
 	(delete-char 1)))))
 
+;; Fixme: Why not use `with-output-to-temp-buffer'?
 (defmacro gnus-with-output-to-file (file &rest body)
   (let ((buffer (make-symbol "output-buffer"))
         (size (make-symbol "output-buffer-size"))
@@ -1103,9 +1107,9 @@ Return the modified alist."
 	   (string-equal (downcase x) (downcase y)))))
 
 (defcustom gnus-use-byte-compile t
-  "If non-nil, byte-compile crucial run-time codes.
-Setting it to nil has no effect after first time running
-`gnus-byte-compile'."
+  "If non-nil, byte-compile crucial run-time code.
+Setting it to nil has no effect after the first time `gnus-byte-compile'
+is run."
   :type 'boolean
   :version "21.1"
   :group 'gnus-various)
@@ -1168,7 +1172,7 @@ If you find some problem with the directory separator character, try
     (- x ?0)))
 
 (defun gnus-url-unhex-string (str &optional allow-newlines)
-  "Remove %XXX embedded spaces, etc in a url.
+  "Remove %XX, embedded spaces, etc in a url.
 If optional second argument ALLOW-NEWLINES is non-nil, then allow the
 decoding of carriage returns and line feeds in the string, which is normally
 forbidden in URL encoding."
@@ -1357,6 +1361,7 @@ Return nil otherwise."
 				 display))
 	      display)))))
 
+;; Fixme: This has only one use (in gnus-agent), which isn't worthwhile.
 (defmacro gnus-mapcar (function seq1 &rest seqs2_n)
   "Apply FUNCTION to each element of the sequences, and make a list of the results.
 If there are several sequences, FUNCTION is called with that many arguments,
@@ -1394,7 +1399,7 @@ sequence, this is like `mapcar'.  With several, it is like the Common Lisp
 (if (fboundp 'merge)
     (defalias 'gnus-merge 'merge)
   ;; Adapted from cl-seq.el
-  (defun gnus-merge (type list1 list2 pred)
+  (defun gnus-merge (list1 list2 pred)
     "Destructively merge lists LIST1 and LIST2 to produce a new list.
 Ordering of the elements is preserved according to PRED, a `less-than'
 predicate on the elements."
