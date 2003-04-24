2ce164f1d823f9de944ff2a5822d0f00251b1c79
Dave Love <fx@gnu.org>
2003-04-24 18:43:57 +0000

    (message-make-date): Autoload rather than requiring message.

--- lisp/gnus-start.el
+++ lisp/gnus-start.el
@@ -32,7 +32,7 @@
 (require 'gnus-spec)
 (require 'gnus-range)
 (require 'gnus-util)
-(require 'message)
+(autoload 'message-make-date "message")
 (eval-when-compile (require 'cl))
 
 (defcustom gnus-startup-file (nnheader-concat gnus-home-directory ".newsrc")
@@ -2884,6 +2884,7 @@ If FORCE is non-nil, the .newsrc file is read."
 		      (or (gnus-group-name-charset method name)
 			  (gnus-parameter-charset name)
 			  gnus-default-charset)))
+		;; Fixme: Don't decode in unibyte mode.
 		(when (and str charset (featurep 'mule))
 		  (setq str (mm-decode-coding-string str charset)))
 		(set group str)))
