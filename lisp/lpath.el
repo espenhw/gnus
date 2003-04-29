8bc28381dd2a97fb640f8b8e56287a223b89f236
Katsumi Yamaoka <yamaoka@jpl.org>
2003-04-29 23:37:03 +0000

    Fbind string-to-multibyte.

--- lisp/lpath.el
+++ lisp/lpath.el
@@ -17,7 +17,7 @@
 	       propertize put-image replace-regexp-in-string
 	       rmail-msg-is-pruned rmail-msg-restore-non-pruned-header
 	       sort-coding-systems spam-BBDB-register-routine
-	       spam-enter-ham-BBDB tool-bar-add-item
+	       spam-enter-ham-BBDB string-to-multibyte tool-bar-add-item
 	       tool-bar-add-item-from-menu tool-bar-local-item-from-menu
 	       url-http-file-exists-p vcard-pretty-print w32-focus-frame
 	       w3m-charset-to-coding-system x-focus-frame))
