fd40157f1d52daafc099fd9f5816b6e55e84e891
Katsumi Yamaoka <yamaoka@jpl.org>
2003-12-25 08:31:17 +0000

    Remove display-time-event-handler and open-ssl-stream; add delete-extent for Emacs; rearrange bindings assuming w3 may not be available and XEmacs without the file-coding feature may be used.

--- lisp/lpath.el
+++ lisp/lpath.el
@@ -11,40 +11,35 @@
 
 (maybe-fbind '(Info-directory
 	       Info-menu bbdb-create-internal bbdb-records create-image
-	       display-graphic-p display-time-event-handler find-coding-system
-	       find-image image-size image-type-available-p insert-image
-	       make-mode-line-mouse-map make-temp-file open-ssl-stream
-	       propertize put-image replace-regexp-in-string
+	       display-graphic-p find-coding-system find-image image-size
+	       image-type-available-p insert-image make-mode-line-mouse-map
+	       make-temp-file propertize put-image replace-regexp-in-string
 	       rmail-msg-is-pruned rmail-msg-restore-non-pruned-header
 	       sort-coding-systems spam-BBDB-register-routine
 	       spam-enter-ham-BBDB string-to-multibyte tool-bar-add-item
 	       tool-bar-add-item-from-menu tool-bar-local-item-from-menu
-	       url-http-file-exists-p vcard-pretty-print w32-focus-frame
-	       w3m-charset-to-coding-system x-focus-frame put-char-table
-	       get-char-table))
+	       url-generic-parse-url url-http-file-exists-p
+	       url-insert-file-contents vcard-pretty-print w32-focus-frame
+	       w3m-charset-to-coding-system w3m-region x-focus-frame))
 (maybe-bind '(filladapt-mode
-	      mc-pgp-always-sign mm-w3m-mode-map rmail-enable-mime-composing
-	      rmail-insert-mime-forwarded-message-function
+	      mc-pgp-always-sign rmail-insert-mime-forwarded-message-function
+	      url-current-object url-package-name url-package-version
+	      w3-meta-charset-content-type-regexp
+	      w3-meta-content-type-charset-regexp
 	      w3m-cid-retrieve-function-alist w3m-current-buffer
 	      w3m-display-inline-images w3m-meta-content-type-charset-regexp
 	      w3m-minor-mode-map))
 
 (if (featurep 'xemacs)
     (progn
-      (maybe-fbind '(Info-directory
-		     Info-menu ccl-execute-on-string char-charset charsetp
-		     coding-system-get coding-system-list coding-system-p
-		     decode-coding-region decode-coding-string
-		     define-ccl-program delete-overlay detect-coding-region
-		     encode-coding-region encode-coding-string
+      (maybe-fbind '(delete-overlay
 		     event-click-count event-end event-start
-		     find-charset-region find-coding-systems-for-charsets
+		     find-coding-systems-for-charsets
 		     find-coding-systems-region find-coding-systems-string
-		     get-charset-property mail-abbrevs-setup
-		     mouse-minibuffer-check mouse-movement-p mouse-scroll-subr
-		     overlay-lists pgg-parse-crc24-string posn-point
-		     posn-window read-event set-buffer-multibyte track-mouse
-		     window-edges w3m-region))
+		     mail-abbrevs-setup mouse-minibuffer-check
+		     mouse-movement-p mouse-scroll-subr overlay-lists
+		     posn-point posn-window read-event set-buffer-multibyte
+		     track-mouse window-edges))
       (maybe-bind '(adaptive-fill-first-line-regexp
 		    buffer-display-table buffer-file-coding-system
 		    current-language-environment
@@ -52,31 +47,31 @@
 		    enable-multibyte-characters gnus-agent-expire-current-dirs
 		    language-info-alist mark-active mouse-selection-click-count
 		    mouse-selection-click-count-buffer pgg-parse-crc24
-		    temporary-file-directory transient-mark-mode
-		    w3-meta-content-type-charset-regexp
-		    w3-meta-charset-content-type-regexp)))
+		    temporary-file-directory transient-mark-mode)))
   (maybe-fbind '(bbdb-complete-name
-		 delete-annotation device-connection dfw-device
+		 delete-annotation delete-extent device-connection dfw-device
 		 events-to-keys font-lock-set-defaults frame-device
-		 glyph-height glyph-width mail-aliases-setup make-annotation
-		 make-event make-glyph make-network-process map-extents
-		 message-xmas-redefine set-extent-property temp-directory
-		 url-generic-parse-url url-insert-file-contents
+		 get-char-table glyph-height glyph-width mail-aliases-setup
+		 make-annotation make-event make-glyph make-network-process
+		 map-extents message-xmas-redefine put-char-table
+		 set-extent-property temp-directory
 		 valid-image-instantiator-format-p
 		 w3-coding-system-for-mime-charset w3-do-setup
-		 w3-prepare-buffer w3-region w3m-region window-pixel-height
+		 w3-prepare-buffer w3-region window-pixel-height
 		 window-pixel-width))
-  (maybe-bind '(help-echo-owns-message
-		mail-mode-hook
-		url-current-object url-package-name url-package-version
-		w3-meta-charset-content-type-regexp
-		w3-meta-content-type-charset-regexp)))
+  (maybe-bind '(help-echo-owns-message mail-mode-hook mm-w3m-mode-map)))
 
 (when (and (featurep 'xemacs)
 	   (not (featurep 'mule)))
-  (progn
-    (maybe-fbind '(coding-system-base find-charset-string))))
-
+  (maybe-fbind '(ccl-execute-on-string
+		 char-charset charsetp coding-system-get define-ccl-program
+		 find-charset-region get-charset-property
+		 pgg-parse-crc24-string))
+  (unless (featurep 'file-coding)
+    (maybe-fbind '(coding-system-base
+		   coding-system-list coding-system-p decode-coding-region
+		   decode-coding-string detect-coding-region
+		   encode-coding-region encode-coding-string))))
 
 (defun nnkiboze-score-file (a)
   )
