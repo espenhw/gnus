bb042e795be35daba285e362c789ce5402c20e0b
Simon Josefsson <jas@extundo.com>
2004-01-04 23:42:24 +0000

    (pgg-passphrase-cache, pgg-run-at-time): Remove. (pgg-add-passphrase-cache, pgg-remove-passphrase-cache): Use the password package.

--- lisp/pgg.el
+++ lisp/pgg.el
@@ -1,6 +1,6 @@
 ;;; pgg.el --- glue for the various PGP implementations.
 
-;; Copyright (C) 1999, 2000, 2003 Free Software Foundation, Inc.
+;; Copyright (C) 1999, 2000, 2003, 2004 Free Software Foundation, Inc.
 
 ;; Author: Daiki Ueno <ueno@unixuser.org>
 ;; Created: 1999/10/28
@@ -29,7 +29,7 @@
 
 (require 'pgg-def)
 (require 'pgg-parse)
-(autoload 'run-at-time "timer")
+(require 'password)
 
 ;; Don't merge these two `eval-when-compile's.
 (eval-when-compile
@@ -88,105 +88,17 @@
 	(set-buffer standard-output)
 	(insert-buffer-substring pgg-errors-buffer)))))
 
-(defvar pgg-passphrase-cache (make-vector 7 0))
-
 (defun pgg-read-passphrase (prompt &optional key)
-  (or (and pgg-cache-passphrase
-	   key (setq key (pgg-truncate-key-identifier key))
-	   (symbol-value (intern-soft key pgg-passphrase-cache)))
-      (read-passwd prompt)))
-
-(eval-when-compile
-  (defvar itimer-process)
-  (defvar itimer-timer)
-  (autoload 'delete-itimer "itimer")
-  (autoload 'itimer-driver-start "itimer")
-  (autoload 'itimer-value "itimer")
-  (autoload 'set-itimer-function "itimer")
-  (autoload 'set-itimer-function-arguments "itimer")
-  (autoload 'set-itimer-restart "itimer")
-  (autoload 'start-itimer "itimer"))
-
-(eval-and-compile
-  (defalias
-    'pgg-run-at-time
-    (if (featurep 'xemacs)
-	(if (condition-case nil
-		(progn
-		  (unless (or itimer-process itimer-timer)
-		    (itimer-driver-start))
-		  ;; Check whether there is a bug to which the difference of
-		  ;; the present time and the time when the itimer driver was
-		  ;; woken up is subtracted from the initial itimer value.
-		  (let* ((inhibit-quit t)
-			 (ctime (current-time))
-			 (itimer-timer-last-wakeup
-			  (prog1
-			      ctime
-			    (setcar ctime (1- (car ctime)))))
-			 (itimer-list nil)
-			 (itimer (start-itimer "pgg-run-at-time" 'ignore 5)))
-		    (sleep-for 0.1) ;; Accept the timeout interrupt.
-		    (prog1
-			(> (itimer-value itimer) 0)
-		      (delete-itimer itimer))))
-	      (error nil))
-	    (lambda (time repeat function &rest args)
-	      "Emulating function run as `run-at-time'.
-TIME should be nil meaning now, or a number of seconds from now.
-Return an itimer object which can be used in either `delete-itimer'
-or `cancel-timer'."
-	      (apply #'start-itimer "pgg-run-at-time"
-		     function (if time (max time 1e-9) 1e-9)
-		     repeat nil t args))
-	  (lambda (time repeat function &rest args)
-	    "Emulating function run as `run-at-time' in the right way.
-TIME should be nil meaning now, or a number of seconds from now.
-Return an itimer object which can be used in either `delete-itimer'
-or `cancel-timer'."
-	    (let ((itimers (list nil)))
-	      (setcar
-	       itimers
-	       (apply #'start-itimer "pgg-run-at-time"
-		      (lambda (itimers repeat function &rest args)
-			(let ((itimer (car itimers)))
-			  (if repeat
-			      (progn
-				(set-itimer-function
-				 itimer
-				 (lambda (itimer repeat function &rest args)
-				   (set-itimer-restart itimer repeat)
-				   (set-itimer-function itimer function)
-				   (set-itimer-function-arguments itimer args)
-				   (apply function args)))
-				(set-itimer-function-arguments
-				 itimer
-				 (append (list itimer repeat function) args)))
-			    (set-itimer-function
-			     itimer
-			     (lambda (itimer function &rest args)
-			       (delete-itimer itimer)
-			       (apply function args)))
-			    (set-itimer-function-arguments
-			     itimer
-			     (append (list itimer function) args)))))
-		      1e-9 (if time (max time 1e-9) 1e-9)
-		      nil t itimers repeat function args)))))
-      'run-at-time)))
+  (when pgg-cache-passphrase
+    (password-read prompt (setq key (pgg-truncate-key-identifier key)))))
 
 (defun pgg-add-passphrase-cache (key passphrase)
-  (setq key (pgg-truncate-key-identifier key))
-  (set (intern key pgg-passphrase-cache)
-       passphrase)
-  (pgg-run-at-time pgg-passphrase-cache-expiry nil
-		   #'pgg-remove-passphrase-cache
-		   key))
+  (let ((password-cache-expiry pgg-passphrase-cache-expiry))
+    (password-cache-add (setq key (pgg-truncate-key-identifier key))
+			passphrase)))
 
 (defun pgg-remove-passphrase-cache (key)
-  (let ((passphrase (symbol-value (intern-soft key pgg-passphrase-cache))))
-    (when passphrase
-      (fillarray passphrase ?_)
-      (unintern key pgg-passphrase-cache))))
+  (password-cache-remove key))
 
 (defmacro pgg-convert-lbt-region (start end lbt)
   `(let ((pgg-conversion-end (set-marker (make-marker) ,end)))
