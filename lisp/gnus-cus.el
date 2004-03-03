8603180be7c05f61d13293bb91a2043eb42d1326
Katsumi Yamaoka <yamaoka@jpl.org>
2004-03-03 05:41:22 +0000

    (gnus-agent-customize-category): Mismatched paren.

--- lisp/gnus-cus.el
+++ lisp/gnus-cus.el
@@ -984,7 +984,7 @@ articles in the thread.
                    (widgets category-fields))
               (while widgets
                 (let* ((widget (pop widgets))
-                       (value (condition-case nil (widget-value widget) (error)))))
+                       (value (condition-case nil (widget-value widget) (error))))
                   (eval `(setf (,(widget-get widget :accessor) ',info)
                                ',value)))))
             (gnus-category-write)
