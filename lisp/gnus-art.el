30a2e74934b56e31df429c0093e0b08d3cffa871
TSUCHIYA Masatoshi <tsuchiya@namazu.org>
2004-05-06 01:41:33 +0000

    (article-strip-banner): Use original from fields instead of MIME-decoded from fields, when checking `gnus-article-address-banner-alist'.

--- lisp/gnus-art.el
+++ lisp/gnus-art.el
@@ -2485,18 +2485,14 @@ always hide."
 	  (article-really-strip-banner
 	   (gnus-parameter-banner gnus-newsgroup-name)))
 	(when gnus-article-address-banner-alist
-	  (article-really-strip-banner
-	   (let ((from (save-restriction
-			 (widen)
-			 (article-narrow-to-head)
-			 (mail-fetch-field "from"))))
-	     (when (and from
-			(setq from
-			      (caar (mail-header-parse-addresses from))))
-	       (catch 'found
-		 (dolist (pair gnus-article-address-banner-alist)
-		   (when (string-match (car pair) from)
-		     (throw 'found (cdr pair)))))))))))))
+	  (let ((from (caar (mail-header-parse-addresses
+			     (gnus-fetch-original-field "from")))))
+	    (when from
+	      (catch 'found
+		(dolist (pair gnus-article-address-banner-alist)
+		  (when (string-match (car pair) from)
+		    (throw 'found
+			   (article-really-strip-banner (cdr pair)))))))))))))
 
 (defun article-really-strip-banner (banner)
   "Strip the banner specified by the argument."
