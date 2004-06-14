bae1debad305e4d16b79442c4e3109d53f1e59d7
Katsumi Yamaoka <yamaoka@jpl.org>
2004-06-14 03:22:18 +0000

    (gnus-article-next-page): Fix the way to find a real end-of-buffer.

--- lisp/gnus-art.el
+++ lisp/gnus-art.el
@@ -5007,6 +5007,7 @@ Argument LINES specifies lines to be scrolled up."
 	      (save-excursion
 		(save-restriction
 		  (widen)
+		  (forward-line)
 		  (eobp)))) ;Real end-of-buffer?
 	  (progn
 	    (when gnus-article-over-scroll
