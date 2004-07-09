521cdccca5161c6f0105b7fa7c0ab069c6de7998
Simon Josefsson <jas@extundo.com>
2004-07-09 10:32:06 +0000

    (gnus-agent-synchronize-flags): Revert to ask. Users will lose all flag changes made while unplugged with e.g. nntp unless flag synchronization happens, thus `nil' is not a good default.  See numerous reports on ding mailing list.

--- lisp/gnus-agent.el
+++ lisp/gnus-agent.el
@@ -113,7 +113,7 @@ If nil, only read articles will be expired."
   :group 'gnus-agent
   :type 'function)
 
-(defcustom gnus-agent-synchronize-flags nil
+(defcustom gnus-agent-synchronize-flags 'ask
   "Indicate if flags are synchronized when you plug in.
 If this is `ask' the hook will query the user."
   :version "21.1"
