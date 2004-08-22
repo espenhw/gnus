22ff2dbbfab7edf90f4e9865c473d90b590a0d0d
Reiner Steib <Reiner.Steib@gmx.de>
2004-08-22 22:06:18 +0000

    [ Merge from Emacs (gnus-5_10-branch): ]
    2004-08-22  Reiner Steib  <Reiner.Steib@gmx.de>
    
    	* gnus-mlspl.el (gnus-group-split-update): Fix docstring.
    
    2004-08-22  Stefan Monnier  <monnier@iro.umontreal.ca>
    
    	* gnus-art.el: Use inhibit-read-only instead of buffer-read-only.
    	(gnus-narrow-to-page): Don't assume point-min == 1.
    	(gnus-article-edit-mode): Derive from message-mode.
    
    	* gnus-score.el (gnus-score-find-bnews): Simplify and don't assume
    	point-min == 1.
    
    	* imap.el (imap-parse-address-list, imap-parse-body-ext):
    	Disable incorrect use of `assert'.
    
    	* message.el (message-mode): Set comment-start-skip.
    
    
    2004-08-22  Sam Steingold  <sds@gnu.org>
    
    	* pop3.el (pop3-leave-mail-on-server): New user variable.
    	(pop3-movemail): Delete mail only when it is nil.

--- lisp/message.el
+++ lisp/message.el
@@ -2455,7 +2455,11 @@ M-RET    `message-newline-and-reformat' (break the line and reformat)."
   (set (make-local-variable 'message-mime-part) 0)
   (message-setup-fill-variables)
   ;; Allow using comment commands to add/remove quoting.
-  (set (make-local-variable 'comment-start) message-yank-prefix)
+  ;; (set (make-local-variable 'comment-start) message-yank-prefix)
+  (when message-yank-prefix
+    (set (make-local-variable 'comment-start) message-yank-prefix)
+    (set (make-local-variable 'comment-start-skip)
+	 (concat "^" (regexp-quote message-yank-prefix) "[ \t]*")))
   (if (featurep 'xemacs)
       (message-setup-toolbar)
     (set (make-local-variable 'font-lock-defaults)
