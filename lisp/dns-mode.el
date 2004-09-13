6b9b5ecb6434cfda73ad286773a6d123ba2596ad
Simon Josefsson <jas@extundo.com>
2004-09-13 11:36:44 +0000

    (dns-mode-font-lock-keywords): Fix faces, reported by Steve Youngs <steve@youngs.au.com> and suggested by Katsumi Yamaoka <yamaoka@jpl.org>.

--- lisp/dns-mode.el
+++ lisp/dns-mode.el
@@ -77,11 +77,11 @@
   "Name of face used for DNS classes, e.g., IN.")
 
 (defcustom dns-mode-font-lock-keywords
-  `(("^$ORIGIN" 0 dns-mode-control-entity-face)
-    ("^$INCLUDE" 0 dns-mode-control-entity-face)
-    ("^$[a-z0-9A-Z]+" 0 dns-mode-bad-control-entity-face)
-    (,(regexp-opt dns-mode-classes) 0 dns-mode-class-face)
-    (,(regexp-opt dns-mode-types) 0 dns-mode-type-face))
+  `(("^$ORIGIN" 0 ,dns-mode-control-entity-face)
+    ("^$INCLUDE" 0 ,dns-mode-control-entity-face)
+    ("^$[a-z0-9A-Z]+" 0 ,dns-mode-bad-control-entity-face)
+    (,(regexp-opt dns-mode-classes) 0 ,dns-mode-class-face)
+    (,(regexp-opt dns-mode-types) 0 ,dns-mode-type-face))
   "Font lock keywords used to highlight text in DNS master file mode."
   :type 'sexp
   :group 'dns-mode)
