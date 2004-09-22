6120bc8f226cb402d61fb27c6c18f27d59ef3315
Katsumi Yamaoka <yamaoka@jpl.org>
2004-09-22 04:40:41 +0000

    (gnus-setup-message): Put article numbers into the X-Draft-From header even if  those articles aren't quoted.

--- lisp/gnus-msg.el
+++ lisp/gnus-msg.el
@@ -406,7 +406,7 @@ Thank you for your help in stamping out bugs.
 		  (not (string= ,group "")))
 	 (push (cons
 		(intern gnus-draft-meta-information-header)
-		(gnus-inews-make-draft ,yanked))
+		(gnus-inews-make-draft (or ,yanked ,article)))
 	       message-required-headers))
        (unwind-protect
 	   (progn
