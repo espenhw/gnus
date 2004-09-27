328888ac66db9d3040a4717e6159c58c9db47959
Simon Josefsson <jas@extundo.com>
2004-09-27 14:29:53 +0000

    Fix license.

--- lisp/hashcash.el
+++ lisp/hashcash.el
@@ -1,14 +1,28 @@
 ;;; hashcash.el --- Add hashcash payments to email
 
-;; $Revision: 1.13 $
+;; $Revision: 7.1 $
 ;; Copyright (C) 1997--2002 Paul E. Foley
 ;; Copyright (C) 2003 Free Software Foundation
 
 ;; Maintainer: Paul Foley <mycroft@actrix.gen.nz>
 ;; Keywords: mail, hashcash
 
-;; Released under the GNU General Public License
-;;   (http://www.gnu.org/licenses/gpl.html)
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
 
 ;;; Commentary:
 
