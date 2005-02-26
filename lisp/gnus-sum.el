e3ed4f7ea1c5851767cf3f76e06cda80e0ac46c4
Miles Bader <miles@gnu.org>
2005-02-26 07:09:08 +0000

    Revision: miles@gnu.org--gnu-2005/gnus--devo--0--patch-57
    Merge from gnus--rel--5.10
    
    Patches applied:
    
     * miles@gnu.org--gnu-2005/emacs--cvs-trunk--0  (patch 132-133)
    
       - Update from CVS
       - Merge from gnus--rel--5.10
    
     * miles@gnu.org--gnu-2005/gnus--rel--5.10  (patch 32)
    
       - Merge from emacs--cvs-trunk--0
    
    2005-02-26  Stefan Monnier  <monnier@iro.umontreal.ca>
    
       * lisp/gnus-sum.el (gnus-summary-exit): Move point after displaying the
       buffer, so it moves the window's cursor.

--- lisp/gnus-sum.el
+++ lisp/gnus-sum.el
@@ -6731,9 +6731,11 @@ If FORCE (the prefix), also save the .newsrc file(s)."
       (set-buffer gnus-group-buffer)
       (if quit-config
 	  (gnus-handle-ephemeral-exit quit-config)
-	(goto-char group-point)
 	(unless leave-hidden
-	  (gnus-configure-windows 'group 'force)))
+	  (gnus-configure-windows 'group 'force))
+	;; Move point after calling gnus-configure-windows to make sure it
+	;; moves the window's point as well.
+	(goto-char group-point))
       ;; Clear the current group name.
       (unless quit-config
 	(setq gnus-newsgroup-name nil)))))
