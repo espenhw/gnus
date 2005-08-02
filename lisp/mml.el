085078309986427c307f807b15bf83a6effb8694
Katsumi Yamaoka <yamaoka@jpl.org>
2005-08-02 00:46:57 +0000

    (mml-generate-mime-1): Make the content type default to text/plain if the  filename is not specified.

--- lisp/mml.el
+++ lisp/mml.el
@@ -413,8 +413,10 @@ If MML is non-nil, return the buffer up till the correspondent mml tag."
 	(let* ((raw (cdr (assq 'raw cont)))
 	       (filename (cdr (assq 'filename cont)))
 	       (type (or (cdr (assq 'type cont))
-			 (and filename (mm-default-file-encoding filename))
-			 "application/octet-stream"))
+			 (if filename
+			     (or (mm-default-file-encoding filename)
+				 "application/octet-stream")
+			   "text/plain")))
 	       (charset (cdr (assq 'charset cont)))
 	       (coding (mm-charset-to-coding-system charset))
 	       encoding flowed coded)
@@ -544,8 +546,10 @@ If MML is non-nil, return the buffer up till the correspondent mml tag."
 	  (insert "\n\n")
 	  (insert "Content-Type: "
 		  (or (cdr (assq 'type cont))
-		      (and name (mm-default-file-encoding name))
-		      "application/octet-stream")
+		      (if name
+			  (or (mm-default-file-encoding name)
+			      "application/octet-stream")
+			"text/plain"))
 		  "\n")
 	  (insert "Content-ID: " (message-make-message-id) "\n")
 	  (insert "Content-Transfer-Encoding: "
