fc35271131b1cf1b63edca9c239d779ad0db3b4e
Reiner Steib <Reiner.Steib@gmx.de>
2005-09-27 17:45:54 +0000

    (mm-automatic-display): Use "text/x-gnus-verbatim" instead of "text/verbatim".

--- lisp/mm-decode.el
+++ lisp/mm-decode.el
@@ -291,7 +291,8 @@ when selecting a different article."
   :group 'mime-display)
 
 (defcustom mm-automatic-display
-  '("text/plain" "text/enriched" "text/richtext" "text/html" "text/verbatim"
+  '("text/plain" "text/enriched" "text/richtext" "text/html"
+    "text/x-gnus-verbatim"
     "text/x-vcard" "image/.*" "message/delivery-status" "multipart/.*"
     "message/rfc822" "text/x-patch" "text/dns" "application/pgp-signature"
     "application/emacs-lisp" "application/x-emacs-lisp"
