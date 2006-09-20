03f6ea6556eab8957cdfffe0be60655f423a220f
Katsumi Yamaoka <yamaoka@jpl.org>
2006-09-20 03:13:15 +0000

    Autoload browse-url-of-file for XEmacs.

--- lisp/dgnushack.el
+++ lisp/dgnushack.el
@@ -201,6 +201,7 @@ fixed in Emacs after 21.3."
     (autoload 'apropos-command "apropos" nil t)
     (autoload 'bbdb-complete-name "bbdb-com" nil t)
     (autoload 'browse-url "browse-url" nil t)
+    (autoload 'browse-url-of-file "browse-url" nil t)
     (autoload 'c-mode "cc-mode" nil t)
     (autoload 'customize-apropos "cus-edit" nil t)
     (autoload 'customize-group "cus-edit" nil t)
